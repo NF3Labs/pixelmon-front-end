71589b9044d3c32214b5ce1691d65c294e6d11bb
andee <andee@nf3x.io>
2023-06-09 21:00:04 +0800

    Updated SDK v1.0.14 for EIP-712 compliance

--- hooks/useSDK.js
+++ hooks/useSDK.js
@@ -12,7 +12,7 @@ export const useSDK = () => {
     try {
       if (data === undefined) {
         const config = { ...Config, JsonRpcProvider: provider };
-        config.contracts = [{ "name": "VAULT", "address": "0x989B2939A5efeba37566112f07eEeD308555C1D8" }, { "name": "NF3Market", "address": "0xDAace31834733322DEa2ffE0562e2b90075A4Af0" }, { "name": "ETH", "address": "0x0000000000000000000000000000000000000000" }];
+        config.contracts = [{ "name": "VAULT", "address": "0xc514b0FDDbdab39fdD9996Ad11f37f2D76cACC2d" }, { "name": "NF3Market", "address": "0xBC881ba2A7D6aFbcd05a4EBA3e646E30E81cAa07" }, { "name": "ETH", "address": "0x0000000000000000000000000000000000000000" }, { "name": "SigningUtils", "address": "0x0C52fE9787e0EEBDC15928Da84b5e3500B7d2035" }];
         
         const frontendSDK = new SDK(config);
         const response = await axios.get('/api/get/getFts')
--- utils/getBackendSDK.js
+++ utils/getBackendSDK.js
@@ -8,11 +8,7 @@ export const getBackendSDK = async () => {
   try {
     if (!backendSDK) {
       const config = { ...Config };
-      config.contracts = [
-        { name: "VAULT", address: "0x989B2939A5efeba37566112f07eEeD308555C1D8" },
-        { name: "NF3Market", address: "0xDAace31834733322DEa2ffE0562e2b90075A4Af0" },
-        { name: "ETH", address: "0x0000000000000000000000000000000000000000" }
-      ];
+      config.contracts = [{ "name": "VAULT", "address": "0xc514b0FDDbdab39fdD9996Ad11f37f2D76cACC2d" }, { "name": "NF3Market", "address": "0xBC881ba2A7D6aFbcd05a4EBA3e646E30E81cAa07" }, { "name": "ETH", "address": "0x0000000000000000000000000000000000000000" }, { "name": "SigningUtils", "address": "0x0C52fE9787e0EEBDC15928Da84b5e3500B7d2035" }];
       const url = ProviderConfig.providerURL + ProviderConfig.providerKey
       const provider = new ethers.providers.JsonRpcProvider(url)
 
